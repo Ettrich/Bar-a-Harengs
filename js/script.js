f12799d160e10cccd7e236da72143f536b4911d1
Filip Tchorek <filip.tchorek@gmail.com>
2018-03-12 13:19:03 +0100

    Fixed smooth scroll

--- js/script.js
+++ js/script.js
@@ -37,7 +37,6 @@ $('a[href*="#"]')
           if ($target.is(":focus")) { // Checking if the target was focused
             return false;
           } else {
-            $target.attr('tabindex','1'); // Adding tabindex for elements not focusable
             $target.focus(); // Set focus again
           };
         });
